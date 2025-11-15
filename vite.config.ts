3d36008eef202e3360dbe8b7ca69a41a1373a70f
David Ackerson <david.ackerson2@gmail.com>
2025-11-15 14:41:43 -0400

    Add node_env for different envs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dackerson.github.io/chez-david/",
+  base: (() => {
+      switch(process.env.NODE_ENV){
+        case 'amplify':
+            return "https://main.dl9hykqwudssf.amplifyapp.com/"
+        default: // github pages
+            return "https://dackerson.github.io/chez-david/"
+      }
+  })()
 })
